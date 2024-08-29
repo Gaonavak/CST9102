6d1bb9868a20f01b5a1b3b499b16a3a6c8bc8ce4
Novak <goghoo857@gmail.com>
2024-08-29 22:15:59 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CST9102
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.Startup += ApplicationStart;
+        }
+
+        private void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            var mainView = new MainWindow();
+            mainView.Show();
+        }
+    }
+}
